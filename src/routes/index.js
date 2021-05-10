be8a68f2c077e1492d8f5ef30cca03dbcab89a13
lambiengcode <lambiengcode@gmail.com>
2021-05-10 15:21:32 +0700

    [test] add route get info server

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,14 @@ const productRoute = require('./productRoute')
 const authRoute = require('./authRoute')
 const categoriesRoute = require('./categoriesRoute')
 const roomRoute = require('./roomRoute')
+const { route } = require('./productRoute')
 const router = express.Router()
 
 router.use('/product', productRoute)
 router.use('/room', roomRoute)
 router.use('/category', categoriesRoute)
 router.use('/auth', authRoute)
+
+router.get('/info', (req, res) => res.status(200).send('Welcome to server aws'))
 
 module.exports = router
