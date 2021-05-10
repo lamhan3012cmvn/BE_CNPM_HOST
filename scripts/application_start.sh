099f9045c8a10599c8708765d69852fe418bcb18
lambiengcode <lambiengcode@gmail.com>
2021-05-10 15:11:36 +0700

    [config] edit config scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,6 +6,9 @@ sudo chmod -R 777 /home/ubuntu/file/be-cnpm
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/file/be-cnpm
 
+# Install yarn
+yarn
+
 # Build docker image
 echo "build docker image..."
 sudo docker build -t lambiengcode/be-cnpm:latest .
